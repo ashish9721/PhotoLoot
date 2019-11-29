490ca7a2a2a16abee97e36ec0c885949b361ae4d
ashish9721 <aashish.sharma@appinventiv.com>
2019-11-29 14:36:11 +0530

    dev branch first  commit

---
+++ src/modules/login/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View,Image,Text} from 'react-native';
+import { styles } from './styles';
+
+export default class SignUp extends React.Component{
+    render(){
+        return(<View style={styles.SignUpContainer}>
+            <Text style={{color:'black',fontSize:30}}>hello</Text>
+        </View>)
+    }
+}
---
+++ src/modules/login/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import {vh,vw,DesignHeight,DesignWidth} from '../../Constants/Dimension'
+
+export const styles =StyleSheet.create({
+SignUpContainer:{
+    flex:1,
+    justifyContent:'center',
+    backgroundColor:'lightblue'
+}
+})
