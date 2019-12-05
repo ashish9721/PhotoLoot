1023e6f535f4615586f660329d62e67f4c6f94a0
ashish9721 <aashish.sharma@appinventiv.com>
2019-12-05 10:41:03 +0530

    commit

--- src/modules/home/currentChallenges.js
+++ src/modules/home/currentChallenges.js
@@ -1,30 +1,18 @@
 
 
 import React, { Component } from 'react'
-<<<<<<< HEAD
-import { Text, View, SafeAreaView, StyleSheet, Button } from 'react-native'
-import { PrizeViews, Description, Rules } from './functional'
-import { vw } from '../../Constants'
-import { StickerAndImage } from '../../component/functionalComponent/functional'
-=======
-<<<<<<< HEAD
-import { Text, View ,SafeAreaView,StyleSheet,Button} from 'react-native'
-import{PrizeViews} from './functional'
-import {ThreeStickerContainer,StickerAndImage} from '../../component/functionalComponent/functional'
-=======
-import { Text, View, SafeAreaView, StyleSheet, Button } from 'react-native'
-import { PrizeViews, Description, Rules } from './functional'
-import { vw } from '../../Constants'
->>>>>>> c4fa14ff0524f02cb0944fd7a104342bd597c9ff
 
->>>>>>> working
+import { Text, View, SafeAreaView, StyleSheet, Button } from 'react-native'
+import { PrizeViews, Description, Rules } from './functional'
+import { vw } from '../../Constants'
+
 // currently working
 export default class CurrentChallenges extends Component {
 
     renderData() {
         return Tempdata.map((result, index) => {
             return (
-                <StickerAndImage heading={result.txt1} 
+                <StickerAndImage key={index} heading={result.txt1} 
                 img={result.img}
                  time={result.txt2}
                   price={result.price}
@@ -39,37 +27,16 @@ export default class CurrentChallenges extends Component {
     render() {
         return (
             <View style={styles.container}>
-<<<<<<< HEAD
-                {
-                    this.renderData()
-                }
-=======
-<<<<<<< HEAD
-           {
-               this.renderData()
-           }
-=======
+
                 <PrizeViews />
                 <Description />
                 <Rules />
->>>>>>> c4fa14ff0524f02cb0944fd7a104342bd597c9ff
->>>>>>> working
             </View>
         )
     }
 }
 
-<<<<<<< HEAD
-const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: 'center' }
 
-=======
-<<<<<<< HEAD
-const styles=StyleSheet.create({
-    container:{flex:1,justifyContent:'center'}
-   
->>>>>>> working
-})
 
 
 const Tempdata = [
@@ -79,7 +46,6 @@ const Tempdata = [
     { txt1: "Beach Happy", txt2: "2 Days Left", price: "500" },
 
 ]
-=======
 const styles = StyleSheet.create({
     container: {
       
@@ -89,4 +55,3 @@ const styles = StyleSheet.create({
     }
 
 })
->>>>>>> c4fa14ff0524f02cb0944fd7a104342bd597c9ff
