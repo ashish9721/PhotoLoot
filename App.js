53f662ba066c684e9ba1b1217e438c3d636ccd9d
ashish9721 <aashish.sharma@appinventiv.com>
2019-12-05 10:21:05 +0530

    App.js commit

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import React from 'react';
-import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
-import SignUp from './src/modules/login/SignUp';
-import Verification from './src/modules/login/verification';
-import CreateAccount from './src/modules/login/createAccount';
-import { Verify } from './src/component/functionalComponent/functional';
-import Home from './src/modules/home/home';
+import React,{Component} from 'react';
 import AppNavigator from './src/route'
 export default class App extends React.Component {
   render() {
     return (
       <AppNavigator />
-   
     )
   }
 }
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   }
-// })
+
