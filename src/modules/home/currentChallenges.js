4ae92ac8b0e89363b1f6f0e1e57f167fb165f486
ashish9721 <aashish.sharma@appinventiv.com>
2019-12-05 11:14:14 +0530

    commit corrected changes

--- src/modules/home/currentChallenges.js
+++ src/modules/home/currentChallenges.js
@@ -1,23 +1,10 @@
 
 
 import React, { Component } from 'react'
-<<<<<<< HEAD
-import { Text, View, SafeAreaView, StyleSheet, Button } from 'react-native'
-import { PrizeViews, Description, Rules } from './functional'
-import { vw } from '../../Constants'
-import { StickerAndImage } from '../../component/functionalComponent/functional'
-=======
-<<<<<<< HEAD
-import { Text, View ,SafeAreaView,StyleSheet,Button} from 'react-native'
-import{PrizeViews} from './functional'
-import {ThreeStickerContainer,StickerAndImage} from '../../component/functionalComponent/functional'
-=======
-import { Text, View, SafeAreaView, StyleSheet, Button } from 'react-native'
-import { PrizeViews, Description, Rules } from './functional'
-import { vw } from '../../Constants'
->>>>>>> c4fa14ff0524f02cb0944fd7a104342bd597c9ff
 
->>>>>>> working
+import { Text, View ,SafeAreaView,StyleSheet,Button} from 'react-native'
+import {ThreeStickerContainer,StickerAndImage} from '../../component/functionalComponent/functional'
+
 // currently working
 export default class CurrentChallenges extends Component {
 
@@ -39,36 +26,19 @@ export default class CurrentChallenges extends Component {
     render() {
         return (
             <View style={styles.container}>
-<<<<<<< HEAD
-                {
-                    this.renderData()
-                }
-=======
-<<<<<<< HEAD
+
            {
                this.renderData()
            }
-=======
-                <PrizeViews />
-                <Description />
-                <Rules />
->>>>>>> c4fa14ff0524f02cb0944fd7a104342bd597c9ff
->>>>>>> working
             </View>
         )
     }
 }
 
-<<<<<<< HEAD
-const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: 'center' }
 
-=======
-<<<<<<< HEAD
 const styles=StyleSheet.create({
     container:{flex:1,justifyContent:'center'}
    
->>>>>>> working
 })
 
 
@@ -79,14 +49,4 @@ const Tempdata = [
     { txt1: "Beach Happy", txt2: "2 Days Left", price: "500" },
 
 ]
-=======
-const styles = StyleSheet.create({
-    container: {
-      
-        justifyContent: 'center',
-        paddingHorizontal:vw(16),
-        // backgroundColor:'red'
-    }
 
-})
->>>>>>> c4fa14ff0524f02cb0944fd7a104342bd597c9ff
